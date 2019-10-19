ba277d912d15fba1531baaf00af0b25035759644
KS <karansinghk0@gmail.com>
2019-10-19 08:33:46 +0530

    edited

--- adSkipper.js
+++ adSkipper.js
@@ -1,8 +1,26 @@
+// setInterval(() => {
+// 	if(performance.navigation.type == 1){
+// 	console.log("page reloaded");
+// }else{
+// 	console.log("not reloaded");
+// }
+// },1000);
+
+// window.onhashchange = () => {
+// 	const video = document.querySelector(".ytp-iv-video-content");
+// 	console.log(video);
+// 	let counter = 0;
+// 	console.log("page reloaded");
+// };
 let looper = setInterval(() => {
-		const btn = document.querySelector(".ytp-ad-skip-button");
-		console.log(btn);
-		if(btn){
-			btn.click();
-			clearInterval(looper);
-		}
-	},3000);
+			const btn = document.querySelector(".ytp-ad-skip-button");
+			// counter++;
+			console.log("no skip button on screen");
+			// if(counter<10){
+				if(btn){
+					btn.click();
+					console.log("clicked!");
+					// clearInterval(looper);
+				}
+			// }	
+		},2000);
