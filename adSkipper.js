db1a1bc434ba5189feaa910299859cecc53c667f
Karan0009 <karansinghk0@gmail.com>
2020-06-07 15:06:29 +0530

    youtube overlay ad close functionality added

--- adSkipper.js
+++ adSkipper.js
@@ -1,26 +1,16 @@
-// setInterval(() => {
-// 	if(performance.navigation.type == 1){
-// 	console.log("page reloaded");
-// }else{
-// 	console.log("not reloaded");
-// }
-// },1000);
-
-// window.onhashchange = () => {
-// 	const video = document.querySelector(".ytp-iv-video-content");
-// 	console.log(video);
-// 	let counter = 0;
-// 	console.log("page reloaded");
-// };
 let looper = setInterval(() => {
-			const btn = document.querySelector(".ytp-ad-skip-button");
-			// counter++;
-			console.log("no skip button on screen");
-			// if(counter<10){
-				if(btn){
-					btn.click();
-					console.log("clicked!");
-					// clearInterval(looper);
-				}
-			// }	
-		},2000);
+  const skipAdBtn = document.querySelector(".ytp-ad-skip-button");
+  const overlayAdBtn = document.querySelector(".ytp-ad-overlay-close-button"); 
+  if (skipAdBtn) {
+    skipAdBtn.click();
+    console.log("ad skipped");
+  }else{
+  	console.log("no skip button on screen");
+  }
+  if(overlayAdBtn){
+  	overlayAdBtn.click();
+  	console.log("ad closed");
+  }else{
+  	console.log("no overlay ad");
+  }
+}, 2000);
